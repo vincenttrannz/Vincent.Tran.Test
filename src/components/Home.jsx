eccee7c44a005abd41e6b50c2c87b5ed84e247e4
Hieu Tran <hieutran@Hieus-Mac-mini.local>
2021-05-30 00:31:26 +1200

    inital commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,36 @@
+import React, {useState} from 'react'
+import Item from './Item'
+import products from '../data/products.json'
+
+export default function Home() {
+    const [state, setState] = useState(products);
+    const sizes = ['XL', 'L', 'M', 'S', 'XS'];
+    const handleChange = (e) => {
+        const selected = e.target.value;
+        const filteredProducts = [];
+        products.map(product => {
+            if (product.size.includes(selected)) filteredProducts.push(product);
+        })
+        // Set new products
+        setState((filteredProducts.length > 0) ? filteredProducts : products);
+    }
+
+    return (
+        <div className="container px-0 my-5">
+            <div className="filterBar row mx-0 mb-2 d-flex justify-content-between align-items-center">
+                <div className="col-12 col-md-9 col-lg-10">
+                    <h1>Women's tops</h1>
+                </div>
+                <div className="col-12 col-md-3 col-lg-2 mb-2 mb-sm-0">
+                    <select onChange={handleChange} className="form-select form-select-sm" aria-label="Filter by size">
+                        <option value="" defaultValue>Filter by size</option>
+                        {sizes.map((size, i) => <option key={i} value={size}>{size}</option>)}
+                    </select>
+                </div>
+            </div>
+            <div className="col-12 d-flex flex-wrap">
+                {state.map((item, i) => <Item key={i} product={item}/>)}
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Item.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default function Item(props) {
+    // console.log(props);
+    const product = props.product;
+    return (
+        <div className="product border col-12 col-sm-6 col-md-4 col-lg-3 px-2 py-2">
+            <img className="col-12" src={`/images/Products/${product.productImage}`} alt={`${product.productName}`} />
+            {
+                (product.isSale) 
+                ? 
+                <div className="product__status is__danger text-center col-3">
+                    <p className="my-0">Sale</p>
+                </div>
+                :
+                (product.isExclusive) 
+                ?
+                <div className="product__status is__success text-center col-4">
+                    <p className="my-0">Exclusive</p>
+                </div>
+                : 
+                <div className="product__status">&nbsp;</div>
+            }
+            <div className="d-flex align-items-center mx-0 mt-3">
+                <div className="col-7 px-0">
+                    <p className="product__name">{product.productName}</p>
+                </div>
+                <div className="col-5 px-0">
+                    <p className="product__price">{product.price}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
